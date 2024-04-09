3009145cecc3eebf504d5f45a891c347940cf7d4
Slowgun <betman321@gmail.com>
2024-04-09 15:49:41 +0900

    Initial Commit

---
+++ wishList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wishList
+//
+//  Created by t2023-m0074 on 4/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
